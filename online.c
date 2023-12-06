32e4187c96b0e033bcc007f138191fd47fd65840
baris_korkmaz <kbaris600@gmail.com>
2023-12-06 21:35:38 +0300

    comments added

--- online.c
+++ online.c
@@ -12,8 +12,8 @@ struct node{
 struct node *buildTree(char *string);
 void preOrder(struct node *Node);
 struct node* insertTree(struct node* p,int keyVal);;
-void turnLeft(struct node **Node);
-void turnRight(struct node **Node);
+void rotateLeft(struct node **Node);
+void rotateRight(struct node **Node);
 
 int search(struct node **Node, int value);
 
@@ -27,10 +27,14 @@ int main(int argc, char *argv[])
 
     while (1)
     {
+        // take a value from user and search it in binary tree every time
         int value;
         printf("\nEnter a value to search:");
         scanf("%d", &value);
+
         search(&root, value);
+
+
         printf("Pre order traversal of constructed tree: ");
         preOrder(root);
     }
@@ -38,33 +42,46 @@ int main(int argc, char *argv[])
 
 int search(struct node **Node, int value)
 {
+    /*
+     * Look for the value in the node.
+     * Return 1 if the value is equal to the key in this node
+     * or a rotation operation has been performed on the node's child,
+     * otherwise 0.
+     */
     if(*Node == NULL)
         return 0;
+
     if((*Node)->key == value)
     {
         (*Node)->freq++;
         return 1;
     }
+
     else if((*Node)->key > value)
     {
         if(search(&(*Node)->left, value))
         {
+            // if it founds value in left subtree check freq value
             if((*Node)->left->freq > (*Node)->freq)
             {
-                turnRight(Node);
+                rotateRight(Node);
                 return 1;
             }
             else
                 return 0;
         }
     }
+
+
     else if((*Node)->key < value)
     {
+
         if(search(&(*Node)->right, value))
         {
+            // if it founds value in right subtree check freq value
             if((*Node)->right->freq > (*Node)->freq)
             {
-                turnLeft(Node);
+                rotateLeft(Node);
                 return 1;
             }
             else
@@ -78,7 +95,7 @@ struct node* insertTree(struct node* p,int keyVal)
     if(p==NULL){
         p=malloc(sizeof(struct node));
         p->key=keyVal;
-        p->freq=0;
+        p->freq=0; // freq value of every node on initialization is 0
         p->right=NULL;
         p->left=NULL;
         return p;
@@ -96,6 +113,11 @@ struct node* insertTree(struct node* p,int keyVal)
 }
 struct node *buildTree(char *fileName)
 {
+    /*
+     * Take values from file.
+     * Insert every node on in order.
+     * Return bst.
+     */
     FILE *fPtr=fopen(fileName,"r");
     char string[50];
     struct node *head= NULL;
@@ -111,24 +133,27 @@ void preOrder(struct node *Node)
 {
     if(Node == NULL)
         return;
+
     printf("(%d,%d) ", Node->key, Node->freq);
     preOrder(Node->left);
     preOrder(Node->right);
 }
 
-void turnLeft(struct node **Node)
+void rotateLeft(struct node **Node)
 {
     struct node *temp = *Node;
     struct node *temp2 = (*Node)->right->left;
+
     *Node = (*Node)->right;
     (*Node)->left = temp;
     (*Node)->left->right = temp2;
 }
 
-void turnRight(struct node **Node)
+void rotateRight(struct node **Node)
 {
     struct node *temp = *Node;
     struct node *temp2 = (*Node)->left->right;
+
     *Node = (*Node)->left;
     (*Node)->right = temp;
     (*Node)->right->left = temp2;
